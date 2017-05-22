6eb74e83cc73367e282dbefe97d2a3a1f83b0815
ecknomichiel <michiel@frostvikenit.eu>
2017-05-22 14:21:47 +0200

    Added SQLServer database

---
+++ CodeAlongMVC/DataAccess/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using CodeAlongMVC.Models;
+
+namespace CodeAlongMVC.DataAccess
+{
+    public class LibraryContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public LibraryContext(): base("DefaultConnection")
+        { }
+    }
+}
