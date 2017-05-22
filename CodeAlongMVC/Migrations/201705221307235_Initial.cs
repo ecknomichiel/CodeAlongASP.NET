db161406c1c60abd2e0ebc15950fd09995a5bf20
ecknomichiel <michiel@frostvikenit.eu>
2017-05-22 15:09:22 +0200

    Added a migration

---
+++ CodeAlongMVC/Migrations/201705221307235_Initial.cs
@@ -0,0 +1,29 @@
+namespace CodeAlongMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        ISBN = c.String(),
+                        Author = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
